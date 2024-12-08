954182c846787da32c0e8c007aa6cbad9dd6ebe7
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-12-08 00:23:51 -0300

    refactor: melhorando a disposição do botão de exemplo de notificação

--- lib/screens/configuracoes.dart
+++ lib/screens/configuracoes.dart
@@ -1,4 +1,5 @@
 import 'package:ez_reminder/components/app_layout.dart';
+import 'package:ez_reminder/components/custom_button.dart';
 import 'package:ez_reminder/components/custom_notification.dart';
 import 'package:ez_reminder/components/titulo.dart';
 import 'package:ez_reminder/global/ezreminder_colors.dart';
@@ -17,6 +18,19 @@ class Configuracoes extends StatelessWidget {
       child: Column(
         children: [
           const Titulo(texto: "Configurações"),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+            child: CustomButton(
+                label: "Exemplo de notificação",
+                onPressed: () {
+                  Provider.of<NotificationService>(context, listen: false)
+                      .showNotificationExample(CustomNotification(
+                          id: 1,
+                          title: "Ir para a faculdade",
+                          body: "Não posso pegar DP",
+                          date: DateTime.now()));
+                }),
+          ),
           ElevatedButton(
             onPressed: () {
               AuthService().deslogarUsuario();
@@ -36,30 +50,6 @@ class Configuracoes extends StatelessWidget {
             ),
             child: const Text(
               "Sair da conta",
-              style: TextStyle(color: Color(0xFFFF0000)),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Provider.of<NotificationService>(context, listen: false)
-                  .showNotificationExample(CustomNotification(
-                      id: 1,
-                      title: "Exemplo de título",
-                      body: "Exemplo de corpo",
-                      date: DateTime.now()));
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color(EzreminderColors.backgroundPreto),
-              textStyle:
-                  const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              elevation: 20,
-              fixedSize: const Size(245, 49),
-              shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(8))),
-            ),
-            child: const Text(
-              "Exemplo de notificação",
               style: TextStyle(color: Color(0xFFFF0000)),
               textAlign: TextAlign.center,
             ),
